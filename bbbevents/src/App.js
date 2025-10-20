41881f35d0b21a00434cc770602d00c03ee66ae0
Abhiram Ramesh <abhiram.ramesh@o9solutions.com>
2025-10-20 09:40:54 +0530

    Moving from BrowserRouter to Hashrouter

--- bbbevents/src/App.js
+++ bbbevents/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Event from './pages/Event';
